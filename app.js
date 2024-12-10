f400a180cc5bab9b2060dc0578b5aa6fba358b6d
Charlaine Berthouze <matheotique64cb@gmail.com>
2024-12-10 15:30:58 +0100

    fix heure et date

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 console.log("hello world");
 
 // Date.now(2)
-// var heureMinutesSeconde = Date.now(2);
+// var heureMinutesSeconde = Date.now(2);5
+
+const jourSemaine = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi' ]
+
+const moisAnnee = [ 'décembre', 'janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre' ] 
+
+
 let dateDuJour = new Date();
 console.log(dateDuJour);
 
 let hours = dateDuJour.getHours();
 console.log(hours + ' heure');
-
 let minutes = dateDuJour.getMinutes();
-console.log(minutes + ' minute');
+document.querySelector("#hour").innerHTML = hours + " : " + minutes;
 
 // resultat: 10:53
 console.log(hours + 'h' + minutes);
@@ -19,14 +24,15 @@ console.log(year + ' année');
 document.querySelector("#year").innerHTML = year;
 
 let day = dateDuJour.getDay();
-console.log(day + ' jour');
+document.querySelector("#day").innerHTML = day;
 
 let month = dateDuJour.getMonth();
 console.log(month + ' mois');
+document.querySelector("#month").innerHTML = month;
 
-let jours = dateDuJour.getUTCDate();
-console.log(jours + ' jours');
-
+let dayOfWeek = dateDuJour.getUTCDate();
+// document.querySelector("#jour").innerHTML = jour;
+document.getElementById("jour").innerHTML = dayOfWeek;
 
 // resultat tuesday 26 december 2024
 console.log(day + ' ' + jours + ' ' + month + ' ' + year);
